067449156a434cd527be6b4cf57b9a961594ba13
Shawn Feldman <shawn@Shawns-MacBook.local>
2016-05-18 19:13:22 -0600

    add regex

--- oauth/index.js
+++ oauth/index.js
@@ -6,14 +6,17 @@ var jwt = require('jsonwebtoken');
 var request = require('request');
 var _ = require('lodash');
 
-var authHeaderRegex = /Bearer (.+)/;
-var PRIVATE_JWT_VALUES = ['application_name', 'client_id', 'api_product_list', 'iat', 'exp'];
+const authHeaderRegex = /Bearer (.+)/;
+const PRIVATE_JWT_VALUES = ['application_name', 'client_id', 'api_product_list', 'iat', 'exp'];
+const SUPPORTED_DOUBLE_ASTERIK_PATTERN = "**";
+const SUPPORTED_SINGLE_ASTERIK_PATTERN = "*";
+const SUPPORTED_SINGLE_FORWARD_SLASH_PATTERN = "/";
 
-module.exports.init = function(config, logger, stats) {
+module.exports.init = function (config, logger, stats) {
 
   var apiKeyCache = {};
 
-  var middleware = function(req, res, next) {
+  var middleware = function (req, res, next) {
 
     var authHeaderName = config['authorization-header'] ? config['authorization-header'] : 'authorization';
     var apiKeyHeaderName = config['api-key-header'] ? config['api-key-header'] : 'x-api-key';
@@ -35,18 +38,18 @@ module.exports.init = function(config, logger, stats) {
         return sendError(req, res, next, logger, stats, 'invalid_request', 'Invalid Authorization header');
       }
       var token = header[1];
-      delete(req.headers[authHeaderName]); // never pass this header to target
+      delete (req.headers[authHeaderName]); // never pass this header to target
 
       verify(token, config, logger, stats, middleware, req, res, next);
     }
   }
 
-  var exchangeApiKeyForToken = function(req, res, next, config, logger, stats, middleware, apiKey) {
+  var exchangeApiKeyForToken = function (req, res, next, config, logger, stats, middleware, apiKey) {
     var cacheControl = req.headers['cache-control'];
     if (!cacheControl || (cacheControl && cacheControl.indexOf('no-cache') < 0)) { // caching is allowed
       var token = apiKeyCache[apiKey];
       if (token) {
-        if (Date.now()/1000 < token.exp) { // not expired yet (token expiration is in seconds)
+        if (Date.now() / 1000 < token.exp) { // not expired yet (token expiration is in seconds)
           debug('api key cache hit', apiKey);
           return authorize(req, res, next, logger, stats, token);
         } else {
@@ -62,16 +65,16 @@ module.exports.init = function(config, logger, stats) {
     request({
       url: config.verify_api_key_url,
       method: 'POST',
-      json: {'apiKey': apiKey},
-      headers: {'x-dna-api-key': apiKey}
-    }, function(err, response, body) {
+      json: { 'apiKey': apiKey },
+      headers: { 'x-dna-api-key': apiKey }
+    }, function (err, response, body) {
       if (err) return sendError(req, res, next, logger, stats, 'gateway_timeout', err.message);
       if (response.statusCode !== 200) return sendError(req, res, next, logger, stats, 'access_denied', response.statusMessage);
       verify(body, config, logger, stats, middleware, req, res, next, apiKey);
     });
   }
 
-  var verify = function(token, config, logger, stats, middleware, req, res, next, apiKey) {
+  var verify = function (token, config, logger, stats, middleware, req, res, next, apiKey) {
     var options = {
       algorithms: ['RS256'],
       ignoreExpiration: false,
@@ -79,7 +82,7 @@ module.exports.init = function(config, logger, stats) {
       issuer: undefined
     };
 
-   jwt.verify(token && token.token ? token.token: token, config.public_key, options, function(err, decodedToken) {
+    jwt.verify(token && token.token ? token.token : token, config.public_key, options, function (err, decodedToken) {
 
       if (err) {
 
@@ -105,17 +108,17 @@ module.exports.init = function(config, logger, stats) {
 
   return {
 
-    onrequest: function(req, res, next) {
+    onrequest: function (req, res, next) {
       middleware(req, res, next);
     },
 
-    api_key_cache_size: function() {
+    api_key_cache_size: function () {
       return Object.keys(apiKeyCache).length;
     },
 
-    api_key_cache_clear: function() {
+    api_key_cache_clear: function () {
       var deleted = 0;
-      Object.keys(apiKeyCache).forEach(function(key) {
+      Object.keys(apiKeyCache).forEach(function (key) {
         delete apiKeyCache[key];
         deleted++;
       });
@@ -125,7 +128,7 @@ module.exports.init = function(config, logger, stats) {
   };
 
   function authorize(req, res, next, logger, stats, decodedToken, apiKey) {
-    if (checkIfAuthorized(config, res.proxy, decodedToken)) {
+    if (checkIfAuthorized(config, req.reqUrl.path, res.proxy, decodedToken)) {
       req.token = decodedToken;
 
       var authClaims = _.omit(decodedToken, PRIVATE_JWT_VALUES);
@@ -135,7 +138,7 @@ module.exports.init = function(config, logger, stats) {
         var cacheControl = req.headers['cache-control'];
         if (!cacheControl || (cacheControl && cacheControl.indexOf('no-cache') < 0)) { // caching is allowed
           // default to now (in seconds) + 30m if not set
-          decodedToken.exp = decodedToken.exp || +(((Date.now()/1000) + 1800).toFixed(0));
+          decodedToken.exp = decodedToken.exp || +(((Date.now() / 1000) + 1800).toFixed(0));
           apiKeyCache[apiKey] = decodedToken;
           debug('api key cache store', apiKey);
         } else {
@@ -153,22 +156,55 @@ module.exports.init = function(config, logger, stats) {
 
 // from the product name(s) on the token, find the corresponding proxy
 // then check if that proxy is one of the authorized proxies in bootstrap
-function checkIfAuthorized(config, proxy, decodedToken) {
+const checkIfAuthorized = module.exports.checkIfAuthorized = function checkIfAuthorized(config, urlPath, proxy, decodedToken) {
 
   if (!decodedToken.api_product_list) { return false; }
 
-  return decodedToken.api_product_list.some(function(product) {
+  return decodedToken.api_product_list.some(function (product) {
 
-    var validProxyNames = config.product_to_proxy[product];
+    const validProxyNames = config.product_to_proxy[product];
     if (!validProxyNames) { return false; }
 
-    return validProxyNames.indexOf(proxy.name) >= 0;
+    const apiproxies = config.product_to_api_resource[product];
+    var matchesProxyRules = false;
+    if(apiproxies && apiproxies.length){
+      apiproxies.forEach(function (apiproxy) {
+          if(matchesProxyRules){
+            //found one
+            return;
+          }
+          if (apiproxy.endsWith("/") && !urlPath.endsWith("/")) {
+              urlPath = urlPath + "/";
+          }
+
+          if(apiproxy.includes(SUPPORTED_DOUBLE_ASTERIK_PATTERN)){
+            const regex = apiproxy.replace(/\*\*/gi,".*")
+            matchesProxyRules =  urlPath.match(regex)
+          }else{
+            if(apiproxy.includes(SUPPORTED_SINGLE_ASTERIK_PATTERN)){
+              const regex = apiproxy.replace(/\*/gi,"[^/]+");
+              matchesProxyRules =  urlPath.match(regex)
+            }else{
+              // if(apiproxy.includes(SUPPORTED_SINGLE_FORWARD_SLASH_PATTERN)){
+              // }
+              matchesProxyRules = urlPath == apiproxy;
+            }
+          }
+      })
+
+    }else{
+      matchesProxyRules = true
+    }
+
+    //add pattern matching here
+
+    return matchesProxyRules &&  validProxyNames.indexOf(proxy.name) >= 0;
   });
 }
 
 function sendError(req, res, next, logger, stats, code, message) {
 
-  switch(code) {
+  switch (code) {
     case 'invalid_request':
       res.statusCode = 400;
       break;
@@ -193,7 +229,7 @@ function sendError(req, res, next, logger, stats, code, message) {
   };
 
   debug('auth failure', res.statusCode, code, message ? message : '', req.headers, req.method, req.url);
-  logger.error({req: req, res: res}, 'oauth');
+  logger.error({ req: req, res: res }, 'oauth');
 
   if (!res.finished) res.setHeader('content-type', 'application/json');
   res.end(JSON.stringify(response));
---
+++ oauth/test/module.js
@@ -0,0 +1,26 @@
+var assert = require('assert')
+var oauth = require('../index.js')
+var config = {
+  "verify_api_key_url":"https://sfeldmanmicro-test.apigee.net/edgemicro-auth/verifyApiKey",
+  "product_to_proxy":{"EdgeMicroTestProduct":["edgemicro_weather"]},
+  "product_to_api_resource":{"EdgeMicroTestProduct":["/hello/blah/*/foo*","/hello/some/**","/hello/blah"]}
+  };
+var proxy = {name:'edgemicro_weather'}
+var token = {api_product_list:['EdgeMicroTestProduct']}
+describe('test oauth',function(){
+  it('checkIfAuthorized',function (done) {
+    var contains;
+    contains = oauth.checkIfAuthorized(config,'/hello',proxy,token);
+    assert(!contains)
+    contains = oauth.checkIfAuthorized(config,'/hello/blah',proxy,token);
+    assert(contains)
+    contains = oauth.checkIfAuthorized(config,'/hello/blah/somerule/foosomething',proxy,token);
+    assert(contains)
+     contains = oauth.checkIfAuthorized(config,'/hello/blah/somerule/ifoosomething',proxy,token);
+    assert(!contains)
+    contains = oauth.checkIfAuthorized(config,'/hello/some/somerule/foosomething',proxy,token);
+    assert(contains)
+    done()
+
+  })
+})
