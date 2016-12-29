31ea0bed99658fe89764721b2ed95ebcb37fdbc3
Sundar Ramamoorthy <sramamoorthy@apigee.com>
2016-12-29 09:56:03 -0800

    Chaned attribute name from created_at_time to created_at_apid

--- apigee_sync.go
+++ apigee_sync.go
@@ -218,7 +218,7 @@ func getBearerToken() bool {
 	req.Header.Set("apid_instance_id", guuid)
 	req.Header.Set("apid_cluster_Id", gapidConfigId)
 	req.Header.Set("status", "ONLINE")
-	req.Header.Set("created_at_time", time.Now().Format(time.RFC3339))
+	req.Header.Set("created_at_apid", time.Now().Format(time.RFC3339))
 	req.Header.Set("plugin_details", gpgInfo)
 
 	client := &http.Client{}
