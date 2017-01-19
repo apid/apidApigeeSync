685d6f06fa20c87fde4afdec7c9ea6e920a9ea3d
alexanderkhimich <akhimich@apigee.com>
2017-01-18 16:49:03 -0800

    Display name changes

--- pluginData.go
+++ pluginData.go
@@ -4,8 +4,8 @@ import "github.com/30x/apid"
 
 var pluginData = apid.PluginData{
 	Name:    "apidApigeeSync",
-	Version: "0.0.2",
+	Version: "0.0.3",
 	ExtraData: map[string]interface{}{
-		"schemaVersion": "0.0.2",
+		"schemaVersion": "0.0.3",
 	},
 }
