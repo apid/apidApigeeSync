8c5474af6f53317e91fd7ef6cbc531afc7420fcf
Matthew Dobson <mdobson4@gmail.com>
2017-02-10 09:53:15 -0500

    Allow for configuration of a mask to didact certain pieces of data from an analytics record.

--- analytics/index.js
+++ analytics/index.js
@@ -9,6 +9,18 @@ module.exports.init = function(config, logger, stats) {
       record.apiproxy = res.proxy.name;
       record.apiproxy_revision = res.proxy.revision;
     }
+
+    if(config.mask) {
+      var maskString = config.mask.mask_string;
+      var fieldsToMask = config.mask.fields_to_mask;
+      fieldsToMask.forEach((f)=> {
+        //Only attempt to mask if the property exists.
+        if(record[f]) {
+          record[f] = maskString;
+        }
+      });
+    }
+    
     cb(null, record);
   };
 
