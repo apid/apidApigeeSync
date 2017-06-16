0eac8ec52bfdf76182c95008d14b2a92b22a968b
Haoming Zhang <zhm1993zhm@gmail.com>
2017-06-16 09:39:48 -0700

    add copyright

--- dockertests/const.go
+++ dockertests/const.go
@@ -1,8 +1,20 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package dockertests
 
 const (
-	dockerCsPort              string = "9000"
-	dockerSsPort              string = "9001"
 	pluginName                       = "apigeeSyncDockerTest"
 	configApidClusterId       = "apigeesync_cluster_id"
 	configProxyServerBaseURI         = "apigeesync_proxy_server_base"
